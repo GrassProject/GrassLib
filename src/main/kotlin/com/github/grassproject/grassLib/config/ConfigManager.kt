70f6a5e3e291c5dc37eea831aa73daaf1ec98d40
apo2073 <apo2073@outlook.com>
2025-03-16 22:05:27 +0900

    YEAPIEE

---
+++ src/main/kotlin/com/github/grassproject/grassLib/config/ConfigManager.kt
@@ -0,0 +1,16 @@
+package com.github.grassproject.grassLib.config
+
+import org.bukkit.configuration.file.YamlConfiguration
+import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
+
+object ConfigManager {
+    fun getConfigFile(plugin: JavaPlugin, file:String):File = File(plugin.dataFolder, file)
+    fun getConfig(file: File):YamlConfiguration = YamlConfiguration.loadConfiguration(file)
+
+    fun setValue(path:String, value:Any, file: File) {
+        val config= getConfig(file)
+        config.set(path, value)
+        config.save(file)
+    }
+}
