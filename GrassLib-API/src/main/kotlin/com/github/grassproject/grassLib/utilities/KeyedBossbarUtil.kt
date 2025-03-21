28ad9d5753e7be3d21a4b95335ccddb100e426ae
wayggstar <wayggstar@gmail.com>
2025-03-21 21:52:53 +0400

    KeyedBossBar

---
+++ GrassLib-API/src/main/kotlin/com/github/grassproject/grassLib/utilities/KeyedBossbarUtil.kt
@@ -0,0 +1,25 @@
+package com.github.grassproject.grassLib.utilities
+
+import org.bukkit.Bukkit
+import org.bukkit.NamespacedKey
+import org.bukkit.boss.BarColor
+import org.bukkit.boss.BarStyle
+import org.bukkit.boss.KeyedBossBar
+import org.bukkit.entity.Player
+import org.bukkit.plugin.java.JavaPlugin
+
+
+fun JavaPlugin.createBossBar(key: String, title: String, color: BarColor, style: BarStyle): KeyedBossBar{
+    val namespacedKey = NamespacedKey(this, key)
+    Bukkit.getBossBar(namespacedKey)?.let { return it }
+
+    return Bukkit.getBossBar(namespacedKey) ?: Bukkit.createBossBar(namespacedKey, title, color, style)
+}
+
+fun KeyedBossBar.removeBossBar(){
+    Bukkit.removeBossBar(this.key)
+}
+
+fun KeyedBossBar.updateProgress(progress: Double){
+    this.progress = progress.coerceIn(0.0, 1.0)
+}
