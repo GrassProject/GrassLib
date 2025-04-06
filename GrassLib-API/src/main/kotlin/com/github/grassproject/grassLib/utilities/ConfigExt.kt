207849a282c75cb65606e767063c2d59c074a351
Jimin Seo <aa090402@naver.com>
2025-04-06 18:14:00 +0900

    add getEnum

---
+++ GrassLib-API/src/main/kotlin/com/github/grassproject/grassLib/utilities/ConfigExt.kt
@@ -0,0 +1,9 @@
+package com.github.grassproject.grassLib.utilities
+
+import org.bukkit.configuration.ConfigurationSection
+
+inline fun <reified T : Enum<T>> ConfigurationSection.getEnum(path: String): T? {
+    return getString(path)?.uppercase()?.let { value ->
+        enumValues<T>().firstOrNull { it.name == value }
+    }
+}
